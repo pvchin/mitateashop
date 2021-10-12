a489f5fbe0d008af088568ce9e1e2bac6a09b11a
pvchine <pvchinbn@yahoo.com>
2021-10-12 12:47:14 +0800

    Oct 12 2nd

--- functions/airtable old.js
+++ functions/airtable old.js
@@ -1,31 +1,31 @@
 // utils/airtable.js
 
-const Airtable = require("airtable");
-const { AIRTABLE_API_ID, AIRTABLE_BASE_ID } = process.env;
-const base = new Airtable({ apiKey: AIRTABLE_API_ID }).base(AIRTABLE_BASE_ID);
-const formattedReturn = require("./formattedReturn");
+// const Airtable = require("airtable");
+// const { AIRTABLE_API_ID, AIRTABLE_BASE_ID } = process.env;
+// const base = new Airtable({ apiKey: AIRTABLE_API_ID }).base(AIRTABLE_BASE_ID);
+// const formattedReturn = require("./formattedReturn");
 
-const table = base("items");
+// const table = base("items");
 
-const getAllItems = async () => {
-  try {
-    const items = await table.select().firstPage();
-    const formattedItems = items.map((rec) => ({
-      id: rec.id,
-      ...rec.fields,
-    }));
+// const getAllItems = async () => {
+//   try {
+//     const items = await table.select().firstPage();
+//     const formattedItems = items.map((rec) => ({
+//       id: rec.id,
+//       ...rec.fields,
+//     }));
 
-    return formattedReturn(200, formattedItems);
-  } catch (err) {
-    console.error(err);
-    return formattedReturn(500, {});
-  }
-};
+//     return formattedReturn(200, formattedItems);
+//   } catch (err) {
+//     console.error(err);
+//     return formattedReturn(500, {});
+//   }
+// };
 
-const transformResponse = (id, fields) => ({
-  id,
-  ...fields,
-});
+// const transformResponse = (id, fields) => ({
+//   id,
+//   ...fields,
+// });
 
-exports.getAllItems = getAllItems;
-//exports.addProject = addProject;
+// exports.getAllItems = getAllItems;
+// //exports.addProject = addProject;
